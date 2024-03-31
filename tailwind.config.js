36943c84a35fcacabafbb3e1abb26adae25aeb1c
sachin22197 <54944580+sachin22197@users.noreply.github.com>
2024-03-31 13:19:59 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  theme: {
+    extend: {
+      colors: {
+        blueHeadingColor: '#282c9b'
+      }
+    },
+  },
+  plugins: [],
+}
